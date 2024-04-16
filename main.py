c9e158d97a8a40c269fd9e00a32659dc969ff155
Juan Páez <jdpluc302@gmail.com>
2024-04-16 13:35:30 -0500

    uncomment parser_data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.asociation_rules import execute_association_rules
 def main() -> None:
     parser = Parser()
     
-    #parser.parse_data() #Uncomment if you want to read de .dbf again.
+    parser.parse_data() #Uncomment if you want to read de .dbf again.
     bill_list, df = parser.to_model()
     parser.show_general_info(df, bill_list)
     execute_association_rules(bill_list)
